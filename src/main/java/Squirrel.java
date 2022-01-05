d242c712785299bc7e5785b21e74c12436fa6187
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-01-05 14:57:23 +0100

    added constructor and eat() to Squirrel.java

--- src/main/java/Squirrel.java
+++ src/main/java/Squirrel.java
@@ -2,5 +2,17 @@ public class Squirrel {
     private double weight;
     private int age;
     private int numOfConesInNest;
-    private boolean hungry;
+    private boolean isHungry;
+
+    public Squirrel(double weight, int age, int numOfConesInNest, boolean isHungry){
+        weight = this.weight;
+        age = this.age;
+        numOfConesInNest = this.numOfConesInNest;
+        isHungry = this.isHungry;
+    }
+
+    public void eat(){
+        numOfConesInNest--;
+        isHungry = false;
+    }
 }
