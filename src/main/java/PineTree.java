60406cad334939d98313a046a0a41d67a5f1e09a
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-01-06 10:25:50 +0100

    changed function to not be static

--- src/main/java/PineTree.java
+++ src/main/java/PineTree.java
@@ -43,7 +43,7 @@ public class PineTree {
             numOfSquirrels--;
         }
     }
-    public static int getNumOfSquirrels(){
+    public int getNumOfSquirrels(){
         return numOfSquirrels;
     }
 
