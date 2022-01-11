7e4b80021b8a26e77300a16eab24c795a18bc717
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-01-11 20:02:43 +0100

    added contructor to Animal

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,3 +1,9 @@
 public class Animal {
+    private int age;
+    private boolean isHungry;
 
+    public Animal(int age, boolean isHungry){
+        this.age = age;
+        this.isHungry = isHungry;
+    }
 }
